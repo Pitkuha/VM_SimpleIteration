7f23de5fef62143cca9ce29f7d18447f41e6a3c1
Pitkuha <Danik2000@bk.ru>
2020-02-24 21:28:31 +0300

    ..

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,37 @@ public class Main {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String otdelitel = ANSI_RED + "===================================================================" + ANSI_RESET;
+
+    static void prettyPrint1(double[] arr) {
+        System.out.println(otdelitel);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(ANSI_GREEN + arr[i] + " " + ANSI_RESET);
+        }
+        System.out.println();
+        System.out.println(otdelitel);
+    }
+    static void prettyPrint2(double[][] arr){
+        System.out.println(otdelitel);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(ANSI_GREEN + arr[i][j] + " " + ANSI_RESET);
+            }
+            System.out.println();
+        }
+        System.out.println(otdelitel);
+    }
 
     public static void main(String[] args) {
-        String otdelitel = ANSI_RED + "===================================================================" + ANSI_RESET;
+
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(otdelitel);
         System.out.println("Program started");
-        System.out.println(otdelitel);
         String variant;
         while (true) {
+            System.out.println(otdelitel);
             System.out.println("Если вы хотите вводить данные с клавиатуры - введите k;");
             System.out.println("Если вы хотитее ввести данные с помощью файла - введите f;");
             System.out.println(otdelitel);
@@ -55,43 +75,38 @@ public class Main {
                 System.out.print("Введите точность:");
                 double t = Double.parseDouble(scanner.nextLine());
 
-                //--------------------------------------------------------
-                for (int i = 0; i < mas.length; i++) {
-                    for (int j = 0; j < mas[0].length; j++) {
-                        System.out.print(ANSI_YELLOW + mas[i][j] + " " + ANSI_RESET);
-                    }
-                    System.out.println();
-                }
-                System.out.println(otdelitel);
-                System.out.println(t);
-                //----------------------------------------------------------
+                prettyPrint2(mas);
+                prettyPrint1(freemas);
             }
             else if (variant.equals("f")) {
                 try {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("iter.txt"), Charset.forName("UTF-8")));
+                    //BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("iter.txt"), Charset.forName("UTF-8")));
+                    BufferedReader reader = new BufferedReader(new FileReader("./src/iter.txt"));
                     int n = Integer.parseInt(reader.readLine());
-                    System.out.println(n);
+                    double[][] mas = new double[n][n];
+                    double[] freemas = new double[n];
+
+                    for (int i = 0; i < n; i++) {
+                        String s = reader.readLine();
+                        double [] numarr = Arrays.stream(s.split(" ")).mapToDouble(Double::parseDouble).toArray();
+                        for (int j = 0; j < n; j++) {
+                        mas[i][j] = numarr[j];
+                        }
+                        freemas[i] = numarr[n];
+                    }
+                    double t = Double.parseDouble(reader.readLine());
+
+                    prettyPrint2(mas);
+                    prettyPrint1(freemas);
                 } catch (IOException e) {
+                    e.printStackTrace();
                     System.out.println(ANSI_RED + "Невозможно извлечь данные из файла" + ANSI_RESET);
+
                 }
             }
             else {
                 System.out.println("Введены неправильные данные");
-                System.out.println(otdelitel);
             }
         }
     }
-
-    public boolean diagonalDominating(double[][] mas, double[] freemas) {
-        double sum = 0;
-        boolean flag = true;
-
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas[0].length; j++) {
-
-            }
-        }
-        return flag;
-    }
-
 }
